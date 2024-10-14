20a8b7ef3f5254d6f37fa05b00d4e25eaa2c4610
Jan <ja.dorn@web.de>
2024-10-14 11:47:12 +0200

    added general layout that every pages can share

--- src/components/pages/intro/page1.vue
+++ src/components/pages/intro/page1.vue
@@ -1,3 +1,38 @@
+<script setup>
+    import PageLayout from '../../layout/PageLayout.vue'
+</script>
+
 <template>
-    das ist ein test
+    <PageLayout>
+        In commodo occaecat est laborum ullamco cillum proident duis proident et commodo.
+        Nulla incididunt consequat sunt eu duis officia irure incididunt.
+        Est sit esse eiusmod tempor ea nulla ut duis voluptate sit.
+        Adipisicing qui est in consectetur eu eu non anim aute sunt tempor dolore magna.
+        Dolore dolor laboris consectetur sint veniam anim quis. 
+        Sunt amet ullamco dolor Lorem labore veniam qui qui cupidatat eiusmod sint in Lorem esse.
+        Veniam duis irure exercitation qui.
+
+        Incididunt occaecat ut mollit adipisicing Lorem enim aliquip aute nulla culpa ut occaecat Lorem velit.
+        Eu officia sit labore do dolore minim consequat anim.
+        Amet Lorem qui veniam velit occaecat cillum reprehenderit sunt irure mollit sunt nostrud est veniam.
+        Do pariatur ullamco reprehenderit sint cupidatat minim aute magna qui quis velit consectetur.
+        Eiusmod voluptate cillum do Lorem excepteur irure dolore.
+        Nulla laborum elit ex adipisicing eu laborum minim excepteur sunt qui voluptate officia aliquip.
+        Velit cupidatat Lorem do adipisicing ut labore id.
+
+        Non sint consequat magna occaecat ullamco excepteur velit proident nulla voluptate non minim.
+        Dolor magna occaecat fugiat magna pariatur aute consequat occaecat cillum est sint aliquip officia.
+        Aliqua commodo pariatur minim nisi commodo nulla dolor ex dolor.
+        Ipsum incididunt enim irure ullamco.
+        Adipisicing eu ullamco et ea commodo aliqua labore consectetur voluptate pariatur ex duis reprehenderit dolore.
+        Ad tempor pariatur fugiat culpa consequat aute excepteur ex. 
+        Qui irure enim quis ad incididunt aliqua esse mollit.
+
+        Aliquip laborum non magna nostrud nisi.
+        Nostrud do eu ullamco esse officia sit irure et quis duis ut culpa tempor.
+        Dolor consectetur exercitation consectetur consectetur velit.
+        Fugiat labore et minim dolor labore eiusmod nostrud velit aliqua.
+        In pariatur reprehenderit sunt anim nulla.
+        Culpa sunt tempor qui sit elit quis aute.
+    </PageLayout>
 </template>
