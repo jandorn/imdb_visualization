7990b95b5e0403eca03593b4a53696e2da3f733c
Jan <ja.dorn@web.de>
2024-10-14 13:51:37 +0200

    breaks in lorem ipsum template

--- src/components/pages/intro/page1.vue
+++ src/components/pages/intro/page1.vue
@@ -10,7 +10,7 @@
         Adipisicing qui est in consectetur eu eu non anim aute sunt tempor dolore magna.
         Dolore dolor laboris consectetur sint veniam anim quis. 
         Sunt amet ullamco dolor Lorem labore veniam qui qui cupidatat eiusmod sint in Lorem esse.
-        Veniam duis irure exercitation qui.
+        Veniam duis irure exercitation qui. <br><br>
 
         Incididunt occaecat ut mollit adipisicing Lorem enim aliquip aute nulla culpa ut occaecat Lorem velit.
         Eu officia sit labore do dolore minim consequat anim.
@@ -18,7 +18,7 @@
         Do pariatur ullamco reprehenderit sint cupidatat minim aute magna qui quis velit consectetur.
         Eiusmod voluptate cillum do Lorem excepteur irure dolore.
         Nulla laborum elit ex adipisicing eu laborum minim excepteur sunt qui voluptate officia aliquip.
-        Velit cupidatat Lorem do adipisicing ut labore id.
+        Velit cupidatat Lorem do adipisicing ut labore id.<br><br>
 
         Non sint consequat magna occaecat ullamco excepteur velit proident nulla voluptate non minim.
         Dolor magna occaecat fugiat magna pariatur aute consequat occaecat cillum est sint aliquip officia.
@@ -26,7 +26,7 @@
         Ipsum incididunt enim irure ullamco.
         Adipisicing eu ullamco et ea commodo aliqua labore consectetur voluptate pariatur ex duis reprehenderit dolore.
         Ad tempor pariatur fugiat culpa consequat aute excepteur ex. 
-        Qui irure enim quis ad incididunt aliqua esse mollit.
+        Qui irure enim quis ad incididunt aliqua esse mollit.<br><br>
 
         Aliquip laborum non magna nostrud nisi.
         Nostrud do eu ullamco esse officia sit irure et quis duis ut culpa tempor.
