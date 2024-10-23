ce12d072cff97e2dbff918d826be73c816e29625
Jan <ja.dorn@web.de>
2024-10-23 21:49:24 +0200

    better wording

--- src/components/pages/placeholder/Genre2.vue
+++ src/components/pages/placeholder/Genre2.vue
@@ -7,8 +7,8 @@ import GenreChart1 from '../../charts/GenreChart1.vue';
   <PageLayout>
     <div class="w-full justify-start text-lg">
       <div>
-        Every movie has one or more  genres associated to it. <br>
-        So let’s add a second dimension to the chart and plot the average rating of all movies of a certain genre.
+        Every movie has one or more <b>genres</b> associated to it. <br>
+        So let’s add a second dimension to the chart and plot the average rating of all movies over all genres.
       </div>
     </div>
     <div class="mt-8 relative">
