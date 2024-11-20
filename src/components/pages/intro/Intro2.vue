773a8529c1a8aad2abdb0272d2a2fb1bc31e9893
Jan <ja.dorn@web.de>
2024-11-20 22:36:32 +0100

    added tooltips and chart transform to this component

--- src/components/pages/intro/Intro2.vue
+++ src/components/pages/intro/Intro2.vue
@@ -10,6 +10,7 @@ const movieStore = useMovieStore();
 const chartRef = ref(null);
 const chartWidth = ref(0);
 const chartHeight = ref(600);
+const mapMode = ref(false);
 
 /*
 const drag = simulation => {
@@ -39,12 +40,49 @@ const drag = simulation => {
 
 let nodes = ref([]);
 let links = ref([]);
-
 let resizeObserver;
+let simulation;
+let colorScale;
+let tooltip;
+let linkLookup = {};
+let svg;
+let matrixGroup;
+let graphGroup;
+
+function updateChartSize() {
+  if (chartRef.value) {
+    chartWidth.value = chartRef.value.clientWidth;
+  }
+}
+
+const initializeScales = () => {
+  const weights = links.value.map(d => d.weight);
+  const minWeight = Math.min(...weights);
+  const maxWeight = Math.max(...weights);
+
+  colorScale = d3.scaleSequential()
+    .domain([minWeight, maxWeight])
+    .interpolator(d3.interpolateYlOrBr);
+
+  if (!tooltip) {
+    tooltip = d3.select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("visibility", "hidden")
+      .style("background-color", "white")
+      .style("border", "1px solid #ddd")
+      .style("border-radius", "4px")
+      .style("padding", "8px")
+      .style("font-size", "12px")
+      .style("pointer-events", "none");
+  }
+};
 
 const computeAdjacencyList = () => {
   const genreOccurrences = {};
   const genrePairs = {};
+  linkLookup = {};
   
   movieStore.movies.forEach(movie => {
     if (movie.genres && movie.genres.length > 0) {
@@ -74,72 +112,43 @@ const computeAdjacencyList = () => {
     const unionCount = genreOccurrences[genre1] + genreOccurrences[genre2] - intersectionCount;
     const jaccardIndex = intersectionCount / unionCount;
 
-    return { 
-      source: genre1, 
-      target: genre2, 
+    const link = {
+      source: genre1,
+      target: genre2,
       weight: jaccardIndex,
       commonMovies: intersectionCount
     };
+
+    const key = [genre1, genre2].sort().join('-');
+    linkLookup[key] = link;
+
+    return link;
   });
 };
 
-watch(() => movieStore.movies, (newMovies) => {
-  if (newMovies.length > 0) {
-    computeAdjacencyList();
-    updateChartSize();
-    renderForceSimulation();
-  }
-}, { immediate: true });
-
-onMounted(() => {
-  updateChartSize();
-  
-  resizeObserver = new ResizeObserver(() => {
-    updateChartSize();
-    renderForceSimulation();
-  });
-
-  if (chartRef.value) {
-    resizeObserver.observe(chartRef.value);
-  }
-
-  if (movieStore.movies.length === 0) {
-    movieStore.fetchMovies();
-  }
-});
-
-onUnmounted(() => {
-  if (resizeObserver) {
-    resizeObserver.disconnect();
-  }
-});
-
-function updateChartSize() {
-  if (chartRef.value) {
-    chartWidth.value = chartRef.value.clientWidth;
-  }
-}
-
 function renderForceSimulation() {
+  initializeScales();
+
   d3.select(chartRef.value).selectAll("svg").remove();
 
-  const svg = d3.select(chartRef.value)
+  svg = d3.select(chartRef.value)
     .append('svg')
     .attr('width', chartWidth.value)
     .attr('height', chartHeight.value);
 
-  const simulation = d3.forceSimulation(nodes.value)
+  graphGroup = svg.append('g')
+    .attr('class', 'graph-group')
+    .attr('transform', `translate(${chartWidth.value / 2}, ${chartHeight.value / 2}) scale(${0.85})`);
+
+  simulation = d3.forceSimulation(nodes.value)
     .force("link", d3.forceLink(links.value)
-        .id(d => d.id)
-        .distance(d => 300 * (1 - d.weight)))
+      .id(d => d.id)
+      .distance(d => 300 * (1 - d.weight)))
     .force("charge", d3.forceManyBody().strength(-200)) 
     .force("center", d3.forceCenter(0, 0))
-    .force("collision", d3.forceCollide().radius(d => Math.sqrt(d.count) * 0.3 + 5)) 
-    .alpha(0.2)  
+    .force("collision", d3.forceCollide().radius(d => Math.sqrt(d.count) * 0.3 + 5))
+    .alpha(0.2)
     .alphaDecay(0.05); 
-
-  const graphGroup = svg.append('g')
-    .attr('transform', `translate(${chartWidth.value / 2}, ${chartHeight.value / 2}) scale(${0.85})`);
 
   const weights = links.value.map(d => d.weight);
   const minWeight = Math.min(...weights);
@@ -149,22 +158,6 @@ function renderForceSimulation() {
     if (minWeight === maxWeight) return 1; 
     return (value - minWeight) / (maxWeight - minWeight);
   };
-
-  const colorScale = d3.scaleSequential()
-    .domain([minWeight, maxWeight])
-    .interpolator(d3.interpolateYlOrBr);
-
-  const tooltip = d3.select("body")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("position", "absolute")
-    .style("visibility", "hidden")
-    .style("background-color", "white")
-    .style("border", "1px solid #ddd")
-    .style("border-radius", "4px")
-    .style("padding", "8px")
-    .style("font-size", "12px")
-    .style("pointer-events", "none");
 
   const link = graphGroup.append("g")
     .selectAll("line")
@@ -191,7 +184,9 @@ function renderForceSimulation() {
   const node = graphGroup.append("g")
     .selectAll("g")
     .data(nodes.value)
-    .join("g")
+    .join("circle")
+    .attr('r', d => Math.sqrt(d.count) * 0.3)
+    .attr("fill", "#F5C519")
     .on("mouseover", (event, d) => {
       tooltip
         .style("visibility", "visible")
@@ -206,16 +201,29 @@ function renderForceSimulation() {
       tooltip.style("visibility", "hidden");
     });
 
-  node.append("circle")
-    .attr('r', d => Math.sqrt(d.count) * 0.3)
-    .attr("fill", "#F5C519");
-
-  node.append("text")
-    .text(d => d.id)
-    .attr("text-anchor", "middle")
-    .attr("dominant-baseline", "central")
-    .attr("font-size", "16px")
-    .attr("fill", "black");
+    const labels = graphGroup.append("g")
+      .selectAll("text")
+      .data(nodes.value)
+      .join("text")
+      .attr('class', 'node-label')
+      .text(d => d.id)
+      .attr("text-anchor", "middle")
+      .attr("dominant-baseline", "central")
+      .attr("font-size", "16px")
+      .attr("fill", "black")
+      .on("mouseover", (event, d) => {
+      tooltip
+          .style("visibility", "visible")
+          .html(`Genre: ${d.id}<br>Count Movies: ${d.count}`);
+      })
+      .on("mousemove", (event) => {
+        tooltip
+          .style("top", (event.pageY - 10) + "px")
+          .style("left", (event.pageX + 10) + "px");
+      })
+      .on("mouseout", () => {
+        tooltip.style("visibility", "hidden");
+      });
 
   simulation.on("tick", () => {
     link
@@ -225,28 +233,216 @@ function renderForceSimulation() {
       .attr("y2", d => d.target.y);
 
     node
-      .attr("transform", d => `translate(${d.x},${d.y})`);
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y);
+
+    labels
+      .attr('x', d => d.x)
+      .attr('y', d => d.y);
   });
 
 }
+
+const renderMatrix = () => {
+  if (matrixGroup) matrixGroup.remove();
+
+  matrixGroup = svg.append('g')
+    .attr('class', 'matrix-group');
+
+  const matrixSize = nodes.value.length;
+  const margin = 22;
+  const cellSize = Math.min(chartWidth.value - margin * 2, chartHeight.value - margin * 2) / (matrixSize + 3);
+  const xOffset = chartWidth.value / 2 - ((matrixSize + 0.5) * cellSize) / 2;
+  const yOffset = chartHeight.value / 2 - ((matrixSize + 0.5) * cellSize) / 2;
+
+  // Matrix-Zellen erstellen
+  const cellsData = nodes.value.flatMap((source, i) => 
+    nodes.value.map((target, j) => {
+      const key = [source.id, target.id].sort().join('-');
+      const link = linkLookup[key];
+
+      let commonMovies, weight;
+
+      if (source.id === target.id) {
+        commonMovies = source.count;
+        weight = source.count;
+      } else {
+        commonMovies = link ? link.commonMovies : 0;
+        weight = link ? link.weight : 0;
+      }
+
+      return {
+        source,
+        target,
+        x: margin + j * (cellSize + 1),
+        y: margin + i * (cellSize + 1),
+        commonMovies: link ? link.commonMovies : 0,
+        weight: link ? link.weight : 0
+      };
+    })
+  );
+
+  matrixGroup.selectAll('.matrix-cell')
+    .data(cellsData)
+    .join('rect')
+    .attr('class', 'matrix-cell')
+    .attr('x', d => d.x + xOffset)
+    .attr('y', d => d.y + yOffset)
+    .attr('width', cellSize)
+    .attr('height', cellSize)
+    .attr('fill', d => d.weight ? colorScale(d.weight) : '#eee')
+    .attr('opacity', 0)
+    .on("mouseover", (event, d) => {
+      tooltip
+        .style("visibility", "visible")
+        .html(`${d.source.id} - ${d.target.id}<br>Gemeinsame Filme: ${d.commonMovies}`);
+    })
+    .on("mousemove", (event) => {
+      tooltip
+        .style("top", (event.pageY - 10) + "px")
+        .style("left", (event.pageX + 10) + "px");
+    })
+    .on("mouseout", () => {
+      tooltip.style("visibility", "hidden");
+    })
+    .transition()
+    .duration(1000)
+    .attr('opacity', 1);
+};
+
+
+const toggleMap = () => {
+  mapMode.value = !mapMode.value;
+  switchViz();
+};
+
+const switchViz = () => {
+  const svg = d3.select(chartRef.value).select('svg');
+  const graphGroup = svg.select('g');
+
+  const matrixSize = nodes.value.length;
+  const cellSize = Math.min(chartWidth.value, chartHeight.value) / (matrixSize + 2);
+  const margin = cellSize * 2;
+  const xOffset = chartWidth.value / 2 - (matrixSize * cellSize) / 2;
+  const yOffset = chartHeight.value / 2 - ((matrixSize + 4) * cellSize) / 2;
+
+  if (mapMode.value) {
+    if (simulation) simulation.stop();
+
+    // Kanten ausblenden
+    graphGroup.selectAll('line')
+      .transition()
+      .duration(1000)
+      .attr('opacity', 0)
+      .remove();
+
+    let endTransitionCalled = false;
+    // Kreise ausblenden
+    graphGroup.selectAll('circle')
+      .transition()
+      .duration(1000)
+      .attr('r', 0)
+      .remove()
+      .on('end', () => {
+        if (!endTransitionCalled) {
+          endTransitionCalled = true;
+          // Labels zur linken Achse bewegen
+          const rowLabels = graphGroup.selectAll('.node-label')
+            .transition()
+            .duration(1000)
+            .attr('x', (d, i) => (margin - 50 + xOffset) - (chartWidth.value / 2))
+            .attr('y', (d, i) => (margin + i * (cellSize+2.2) + cellSize / 2 + yOffset + 8) - (chartHeight.value / 2))
+            .attr('text-anchor', 'end')
+            .attr('alignment-baseline', 'middle')
+            .attr('font-size', '12px');
+  
+          const columnLabels = graphGroup.selectAll('.node-label')
+            .clone(true)
+            .attr('class', 'column-label')
+            .attr('x', d => d.x)
+            .attr('y', d => d.y)
+            .transition()
+            .duration(1000)
+            .attr('x', (d, i) => (margin + i * (cellSize+2.2) + xOffset + cellSize / 2) - (chartWidth.value / 2))
+            .attr('y', (margin - 5 + yOffset - 40) - (chartHeight.value / 2))
+            .attr('transform', (d, i) => {
+              const x = (margin + i * (cellSize+2.2) + xOffset + cellSize / 2) - (chartWidth.value / 2);
+              const y = (margin - 5 + yOffset) - (chartHeight.value / 2);
+              return `rotate(-90, ${x}, ${y})`;
+            })
+            .attr('text-anchor', 'start')
+            .attr('alignment-baseline', 'hanging')
+            .attr('font-size', '12px');
+        }
+      });
+
+    // Matrix nach den Transitionen rendern
+    setTimeout(() => {
+      renderMatrix();
+    }, 1000);
+
+  } else {
+    // Matrix und Labels ausblenden
+    if (matrixGroup) {
+      matrixGroup.remove();
+    }
+    graphGroup.selectAll('.column-label').remove();
+
+    // Force Layout neu rendern
+    setTimeout(() => {
+      renderForceSimulation();
+    });
+  }
+};
+
+
+onMounted(() => {
+  updateChartSize();
+  
+  resizeObserver = new ResizeObserver(() => {
+    updateChartSize();
+    renderForceSimulation();
+  });
+
+  if (chartRef.value) {
+    resizeObserver.observe(chartRef.value);
+  }
+
+  if (movieStore.movies.length === 0) {
+    movieStore.fetchMovies();
+  }
+});
+
+onUnmounted(() => {
+  if (resizeObserver) {
+    resizeObserver.disconnect();
+  }
+});
+
+watch(() => movieStore.movies, (newMovies) => {
+  if (newMovies.length > 0) {
+    computeAdjacencyList();
+    updateChartSize();
+    renderForceSimulation();
+  }
+}, { immediate: true });
 
 </script>
 
 <template>
   <PageLayout>
-    <div v-if="movieStore.isLoading">Daten werden geladen...</div>
-    <div v-else-if="movieStore.error">Fehler beim Laden der Daten: {{ movieStore.error }}</div>
+    <div v-if="movieStore.isLoading">Loading...</div>
+    <div v-else-if="movieStore.error">Error: {{ movieStore.error }}</div>
     <div v-else>
-      <div class="flex justify-center w-full">
+      <div class="flex flex-col items-center w-full">
+        <button 
+          @click.stop="toggleMap" 
+          class="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          {{ mapMode ? 'Show Force Layout' : 'Show Matrix' }}
+        </button>
         <div ref="chartRef" class="w-full h-[600px]"></div>
       </div>
-      In commodo occaecat est laborum ullamco cillum proident duis proident et commodo.
-      Nulla incididunt consequat sunt eu duis officia irure incididunt.
-      Est sit esse eiusmod tempor ea nulla ut duis voluptate sit.
-      Adipisicing qui est in consectetur eu eu non anim aute sunt tempor dolore magna.
-      Dolore dolor laboris consectetur sint veniam anim quis. 
-      Sunt amet ullamco dolor Lorem labore veniam qui qui cupidatat eiusmod sint in Lorem esse.
-      Veniam duis irure exercitation qui. <br><br>
     </div>
   </PageLayout>
 </template>
